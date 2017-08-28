323025a4bad271a111529753eb2b7a4df6719cb6
jokeum <joke0111@gmail.com>
2017-08-28 17:10:52 +0800

    add 404 page for redirecting on static gh-pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
 import Landing from './containers/Landing'
 import registerServiceWorker from './registerServiceWorker';
 
