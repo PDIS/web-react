d303d7590162684ff6cb6c815999fbd25e3424ef
jokeum <joke0111@gmail.com>
2017-08-29 17:21:26 +0800

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-// import App from './App';
-import Landing from './containers/Landing'
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Landing />, document.getElementById('root'));
-registerServiceWorker();
