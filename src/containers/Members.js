49db56a3ccb8a7e0f5821ed9fbbecaad6285101a
XanxusX <tako30215@hotmail.com>
2017-09-01 01:32:39 +0800

    add first member template

--- src/containers/Members.js
+++ src/containers/Members.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './Member.css'
-/* import '../styles/template/animate.css'
-import '../styles/template/icomoon.css'
-import '../styles/template/bootstrap.css'
-import '../styles/template/superfish.css'
-import '../styles/template/style.css' */
-
 
 class Members extends React.Component {
   constructor(props) {
@@ -30,120 +24,70 @@ class Members extends React.Component {
         person['id'] = user.id;
         if (user.name == '') {
           person['name'] = user.username;
-        }
-        else {
+        } else {
           person['name'] = user.name;
         }
         person['username'] = user.username;
-        person['image'] = 'https://talk.pdis.nat.gov.tw/' + user.avatar_template.replace("{size}", "800");
+        person['image'] = 'https://talk.pdis.nat.gov.tw/' + user
+          .avatar_template
+          .replace("{size}", "800");
         person['wiselikelink'] = "https://wiselike.tw/#/user/" + user.username;
         let infos = await fetch("https://talk.pdis.nat.gov.tw/c/wiselike/profile-" + user.username.toString().toLowerCase().replace(/_/g, '-') + ".json")
         let info = await infos.json()
         let topic = info.topic_list.topics[0];
-        var content = topic.excerpt.replace(" :new:", "");
+        var content = topic
+          .excerpt
+          .replace(" :new:", "");
         if (content.includes("建立一個完整的")) {
           person['description'] = "";
-        }
-        else {
+        } else {
           person['description'] = content;
         }
         people.push(person);
       }
       //console.log(member)
-      this.setState({ members: people })
-    }
-    catch (e) {
+      this.setState({members: people})
+    } catch (e) {
       console.log(e)
     }
   }
 
   render() {
     return (
-      /* <div className="wrapper row2">
-       <section className="hoc container clear">
-         <div className="sectiontitle">
-           <h6 className="heading">Members</h6>
-         </div>
-         <div className="group">
-         {this.state.members.map((member) => (
-           <div key={member.id} className="one_third"><a className="imgover" href="#"><img src={member.image} alt="" /></a></div>
-         ))}
-         </div>
-       </section>
-     </div>  */
       <div id="fh5co-work-section">
         <div className="container">
           <div className="row">
-            <div className="col-md-8 col-md-offset-2 text-center heading-section animate-box">
+            <div
+              className="col-md-8 col-md-offset-2 text-center heading-section animate-box">
               <h3>Outstanding Projects</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit est facilis maiores, perspiciatis accusamus asperiores sint consequuntur debitis.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit est facilis
+                maiores, perspiciatis accusamus asperiores sint consequuntur debitis.</p>
             </div>
           </div>
         </div>
         <div className="container">
           <div className="row">
-            <div className="col-md-4">
-              <div className="fh5co-grid animate-box" style={{'backgroundImage': 'url(images/work-1.jpg)'}}>
-                <a className="image-popup text-center" href="#">
-                  <div className="prod-title">
-                    <h3>Don’t Just Stand There</h3>
-                    <span>Illustration, Print</span>
+            {this
+              .state
+              .members
+              .map((member) => (
+                <div className="col-md-4" key={member.id}>
+                  <div
+                    className="fh5co-grid animate-box"
+                    style={{
+                    'backgroundImage': 'url(' + member.image + ')'
+                  }}>
+                    <a className="image-popup text-center" href={member.wiselikelink}>
+                      <div className="prod-title">
+                        <h3>{member.name}</h3>
+                        <br/>
+                        <span>{member.description}</span>
+                      </div>
+                    </a>
                   </div>
-                </a>
-              </div>
-            </div>
-         {/*    <div className="col-md-4">
-              <div className="fh5co-grid animate-box" style="background-image: url(images/work-2.jpg);">
-                <a className="image-popup text-center" href="#">
-                  <div className="prod-title">
-                    <h3>Don’t Just Stand There</h3>
-                    <span>Illustration, Print</span>
-                  </div>
-                </a>
-              </div>
-            </div>
-            <div className="col-md-4">
-              <div className="fh5co-grid animate-box" style="background-image: url(images/work-3.jpg);">
-                <a className="image-popup text-center" href="#">
-                  <div className="prod-title">
-                    <h3>Don’t Just Stand There</h3>
-                    <span>Illustration, Print</span>
-                  </div>
-                </a>
-              </div>
-            </div>
-            <div className="col-md-4">
-              <div className="fh5co-grid animate-box" style="background-image: url(images/work-4.jpg);">
-                <a className="image-popup text-center" href="#">
-                  <div className="prod-title">
-                    <h3>Don’t Just Stand There</h3>
-                    <span>Illustration, Print</span>
-                  </div>
-                </a>
-              </div>
-            </div>
-            <div className="col-md-4">
-              <div className="fh5co-grid animate-box" style="background-image: url(images/work-5.jpg);">
-                <a className="image-popup text-center" href="#">
-                  <div className="prod-title">
-                    <h3>Don’t Just Stand There</h3>
-                    <span>Illustration, Print</span>
-                  </div>
-                </a>
-              </div>
-            </div>
-            <div className="col-md-4">
-              <div className="fh5co-grid animate-box" style="background-image: url(images/work-6.jpg);">
-                <a className="image-popup text-center" href="#">
-                  <div className="prod-title">
-                    <h3>Don’t Just Stand There</h3>
-                    <span>Illustration, Print</span>
-                  </div>
-                </a>
-              </div>
-            </div> */}
+                </div>
+              ))}
           </div>
-
         </div>
       </div>
     );
