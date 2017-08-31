7a3b119a8a1856f8d2b838ee1739a92deebbb1a8
XanxusX <tako30215@hotmail.com>
2017-09-01 02:00:51 +0800

    add tag

--- src/containers/Tracking.js
+++ src/containers/Tracking.js
@@ -13,7 +13,7 @@ class Tracking extends React.Component {
       total: [],
       q: '',
       more_url: '',
-      autoLoad: true,
+      autoLoad: true
     };
   }
 
@@ -24,75 +24,132 @@ class Tracking extends React.Component {
     let categories = YAML.parse(raw);
     return categories;
   }
+  async getIds(q, more_url) { //取得討論區每篇文的ID & more_url
+    /* fetch date base on if 'q' & 'more_url' exist */
+    /*
+        https://talk.pdis.nat.gov.tw/c/pdis-site/how-we-work-track
+        /c/pdis-site/how-we-work-track/l/latest?page=1
+        https://talk.pdis.nat.gov.tw/tags/c/pdis-site/how-we-work-track/TAG
+        /tags/c/pdis-site/how-we-work-track/TAG/l/latest?page=1
+    */
+    let query
+    if (q) {
+      if (more_url) {
+        query = Discourselink.Host + more_url.replace(/latest/, 'latest.json')
+      } else {
+        query = Discourselink.Host + Discourselink.Tags + Discourselink.Category + Discourselink.HOWWEWORKTRACK + '/' + q + Discourselink.Filename
+      }
+    } else {
+      if (more_url) {
+        query = Discourselink.Host + more_url.replace(/latest/, 'latest.json')
+      } else {
+        query = Discourselink.Host + Discourselink.Category + Discourselink.HOWWEWORKTRACK + Discourselink.Filename
+      }
+    }
 
-  async goAnchor(anchor) {
-    // console.log(anchor)
-    if (anchor == "top") {
-      /* go to top */
-      $('html, body').animate({
-        scrollTop: 0,
-      }, 300)
-    }
-    else if (anchor) {
-      /* get the top position of anchor */
-      let anchor_y = $(anchor).offset().top
-      /* go to anchor (animation to do) */
-      $('html, body').animate({
-        scrollTop: anchor_y,
-      }, 300)
-    }
-    return false
+    return this
+      .http
+      .get(query)
+      /* to get more_url */
+      .do 
+        (data => this.more_url = data.json().topic_list.more_topics_url || '')
+        /* to get ids */
+          .map(data => data.json().topic_list.topics.map(topic => topic['id']))
+      }
+      async goAnchor(anchor) {
+        // console.log(anchor)
+        if (anchor == "top") {
+          /* go to top */
+          $('html, body').animate({
+            scrollTop: 0
+          }, 300)
+        } else if (anchor) {
+          /* get the top position of anchor */
+          let anchor_y = $(anchor)
+            .offset()
+            .top
+          /* go to anchor (animation to do) */
+          $('html, body').animate({
+            scrollTop: anchor_y
+          }, 300)
+        }
+        return false
+      }
+
+      async componentDidMount() {
+        try {
+          let tagdata = await fetch("https://talk.pdis.nat.gov.tw/tags/filter/search.json");
+          let data = await tagdata.json();
+          let tags = [];
+          let discourseTags = data['results'];
+          for (var i in discourseTags) {
+            if (discourseTags[i]['text'] === '尚未回覆') {
+              continue;
+            }
+            if (/^(.+)-/.test(discourseTags[i]['text'])) {
+              continue;
+            }
+            let tag = {};
+            tag['text'] = discourseTags[i]['text'];
+            tag['weight'] = discourseTags[i]['count'];
+            tag['link'] = "/how-we-work/tracks?q=" + discourseTags[i]['text'];
+            tags.push(tag);
+          }
+          this.setState({tags: tags})
+          /* init categories tab header */
+          let Cats = await this.getCategories();
+          if (this.state.total.length > 0) 
+            return
+          this
+            .state
+            .total
+            .push({category: 'All', posts: new Array()})
+          Object
+            .keys(Cats)
+            .forEach(key => this.state.total.push({category: key, posts: new Array()}))
+          this
+            .state
+            .total
+            .push({category: 'Other', posts: new Array()})
+          /* Tag Query & Timeline */
+          let categories = Cats;
+          let url = this.props.location.search;
+          if (url != '') {
+            let urlparams = url.replace('?q=', '');
+            this.setState({q: urlparams});
+          } else {
+            this.setState({q: ''});
+          }
+          if (this.state.q) {
+            //this.goAnchor('#cloud');
+          }
+          this
+            .state
+            .total
+            .forEach(t => t.posts = []);
+        } catch (e) {
+          console.log(e)
+        }
+      }
+
+      render() {
+        return (
+          <div>
+            <ul className="cloud" id="cloud">
+              {this
+                .state
+                .tags
+                .map(t => (
+                  <li key={t.text} className='cloud-item'>
+                    <a href={t.link} data-weight={t.weight}>
+                      {t.text}
+                    </a>
+                  </li>
+                ))}
+            </ul>
+          </div>
+        );
+      }
   }
 
-  async componentDidMount() {
-    try {
-      let tagdata = await fetch("https://talk.pdis.nat.gov.tw/tags/filter/search.json");
-      let data = await tagdata.json();
-      let tags = [];
-      let discourseTags = data['results'];
-      for (var i in discourseTags) {
-        if (discourseTags[i]['text'] === '尚未回覆') { continue; }
-        if (/^(.+)-/.test(discourseTags[i]['text'])) { continue; }
-        let tag = {};
-        tag['text'] = discourseTags[i]['text'];
-        tag['weight'] = discourseTags[i]['count'];
-        tag['link'] = "/how-we-work/tracks?q=" + discourseTags[i]['text'];
-        tags.push(tag);
-      }
-      this.setState({ tags: tags })
-      /* init categories tab header */
-      let Cats = await this.getCategories();
-      if (this.state.total.length > 0) return
-      this.state.total.push({ category: 'All', posts: new Array() })
-      Object.keys(Cats).forEach(key => this.state.total.push({ category: key, posts: new Array() }))
-      this.state.total.push({ category: 'Other', posts: new Array() })
-      /* Tag Query & Timeline */
-      let categories = Cats;
-      let url = this.props.location.search;
-      if (url != '') {
-        let urlparams = url.replace('?q=', '');
-        this.setState({ q: urlparams });
-      }
-      else {
-        this.setState({ q: '' });
-      }
-      if (this.state.q) {
-        //this.goAnchor('#cloud');
-      }
-      this.total.forEach(t => t.posts = []);
-    }
-    catch (e) {
-      console.log(e)
-    }
-  }
-
-  render() {
-    return (
-      <div>
-
-      </div>
-    );
-  }
-}
-
-export default Tracking;
+  export default Tracking;
