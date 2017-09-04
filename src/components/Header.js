c3d721ebb32d848524c0e0de521921db80cb533a
jokeum <joke0111@gmail.com>
2017-09-04 18:10:32 +0800

    add sider to thin-only view

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-import { Dropdown, Menu, Icon } from 'antd'
+import { Dropdown, Menu, Icon, Button } from 'antd'
 import clearlogo from '../images/pdis-logo-final-clear.png'
 import './Header.css'
+
+const SubMenu = Menu.SubMenu;
 
 const menuWork = (
   <Menu>
@@ -38,6 +40,18 @@ const menuHow = (
 )
 
 class Head extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      collapsed: true,
+    }
+  }
+  toggleCollapsed = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    })
+  }
+
   render() {
     return (
       <header className="header-wrapper">
@@ -50,7 +64,7 @@ class Head extends React.Component {
           </Link>
         </div>
 
-        <nav className="mainav">
+        <nav className="fat-nav fat-only">
           <ul>
             <li>
               <p>
@@ -81,6 +95,52 @@ class Head extends React.Component {
               </p>
             </li>
           </ul>
+        </nav>
+
+        <div className="thin-only">
+          <Button ghost type="primary" onClick={this.toggleCollapsed}>
+            <Icon type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'} />
+          </Button>
+        </div>
+
+        <nav className="thin-nav thin-only" style={{'left': (this.state.collapsed ? '-99vw' : '0' )}}>
+          <Menu
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+            mode="inline"
+            theme="dark"
+            onClick={this.toggleCollapsed}
+          >
+            <Menu.Item key="0">
+              <Icon type="cross" />
+            </Menu.Item>
+            <Menu.Item key="1">
+              <Icon type="pie-chart" />
+              <span>Option 1</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="desktop" />
+              <span>Option 2</span>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <Icon type="inbox" />
+              <span>Option 3</span>
+            </Menu.Item>
+            <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
+              <Menu.Item key="5">Option 5</Menu.Item>
+              <Menu.Item key="6">Option 6</Menu.Item>
+              <Menu.Item key="7">Option 7</Menu.Item>
+              <Menu.Item key="8">Option 8</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub2" title={<span><Icon type="appstore" /><span>Navigation Two</span></span>}>
+              <Menu.Item key="9">Option 9</Menu.Item>
+              <Menu.Item key="10">Option 10</Menu.Item>
+              <SubMenu key="sub3" title="Submenu">
+                <Menu.Item key="11">Option 11</Menu.Item>
+                <Menu.Item key="12">Option 12</Menu.Item>
+              </SubMenu>
+            </SubMenu>
+          </Menu>
         </nav>
 
       </header>
